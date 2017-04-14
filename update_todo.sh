b8da0c6dedf7f9a025862970f2b8771c6eb5c85f
Kent Fredric <kentfredric@gmail.com>
2017-04-14 18:32:24 +1200

    Update generation logic

--- update_todo.sh
+++ update_todo.sh
@@ -1,3 +1,5 @@
 for i in a b c d e f g h i j k l m n o p q r s t u v w x y z; do
   perl stabile-ring-todo.pl dev-perl-${i} > todo/dev-perl-${i}
+  chown root:wheel todo/dev-perl-${i}
+  chmod u+rw,g+rw todo/dev-perl-${i}
 done
