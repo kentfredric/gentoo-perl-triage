8f98343d5a1bb1143d0f923836a233f77a4789ef
Kent Fredric <kentfredric@gmail.com>
2017-05-03 13:22:27 +1200

    Make todolists more xargs friendly

--- stabile-ring-todo.pl
+++ stabile-ring-todo.pl
@@ -7,11 +7,11 @@ open my $fh, '<', $ARGV[0] or die "Can't open $ARGV[0], $!";
 while ( my $line = <$fh> ) {
   chomp $line;
   if ( $line =~ /^##/ ) {
-    print "$line\n";
     next;
   }
   my ( $package, $status, $whiteboard ) = split /(?<=[^#])\s+#/, $line;
   next unless not defined $whiteboard or $whiteboard =~ /\A\s*\z/;
-  print "$line\n"; 
+  $package =~ s/^#//;
+  print "$package\n";
 }
 
