3e6a007a1956332a8543bd99759ec148d971658f
Kent Fredric <kentfredric@gmail.com>
2017-05-04 15:03:16 +1200

    todoize while updating

--- update_all.sh
+++ update_all.sh
@@ -1,3 +1,4 @@
+set -euo pipefail
 for i in index.in/*; do
   f="$(basename "${i}")"
   if [[ ! -e "index/${f}" ]]; then
@@ -6,7 +7,9 @@ for i in index.in/*; do
   echo "Merging ${f}"
   perl merge-set.pl "index/${f}" "index.in/${f}" > "/tmp/${f}.out"
   mv "/tmp/${f}.out" "index/${f}"
-  chown root:wheel "index/${f}"
-  chmod u+rw,g+rw "index/${f}"
+  echo "Todoizing ${f}"
+  perl stabile-ring-todo.pl "index/${f}" > "todo/${f}"
+  chown root:wheel "index/${f}" "todo/${f}"
+  chmod u+rw,g+rw "index/${f}" "todo/${f}"
   rm "index.in/${f}"
 done
