0a0601c070d0d1fab749e5a664bcc7793f5d3e8d
Kent Fredric <kentfredric@gmail.com>
2017-06-09 00:21:43 +1200

    wipe p.unmask autounmask files during cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,4 @@
 	truncate -s 0 /etc/portage/package.accept_keywords/zzz-autounmask
 	truncate -s 0 /etc/portage/package.keywords/zzz-autounmask
 	truncate -s 0 /etc/portage/package.use/zzz-autounmask
+	truncate -s 0 /etc/portage/package.unmask/zzz-autounmask
