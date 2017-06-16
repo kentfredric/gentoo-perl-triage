4d966308c696d77c734307820cf4c96345f3ab3a
Kent Fredric <kentfredric@gmail.com>
2017-06-17 06:54:52 +1200

    Remove perl-wip from stat tool

--- stat-commit.sh
+++ stat-commit.sh
@@ -1,10 +1,10 @@
 truncate -s 0 /tmp/.message
 
 master="$( git --no-pager -C ../../usr/portage rev-parse --short remotes/heads/master )"
-perlgit="$(  git --no-pager -C ../../usr/portage rev-parse --short remotes/gentoo-perl/perl-wip )"
+#perlgit="$(  git --no-pager -C ../../usr/portage rev-parse --short remotes/gentoo-perl/perl-wip )"
 stats="$( perl sync.pl stats | grep all | sed 's/^.*all:\s*//' )"
 
-echo "sync to $master + perl-wip $perlgit: $stats" >> /tmp/.message
+echo "sync to $master: $stats" >> /tmp/.message
 echo  >> /tmp/.message
 perl sync.pl stats >> /tmp/.message
 
