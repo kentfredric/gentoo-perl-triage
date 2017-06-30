9ec3e248aca9bf0ab5c607486121913e55bb6c33
Kent Fredric <kentfredric@gmail.com>
2017-06-30 12:45:57 +1200

    cleanup: remerge world before depclean with unmasks reset

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
-	emerge --depclean -q --with-bdeps=y
-	truncate -s 0 /etc/portage/package.accept_keywords/zzz-autounmask
-	truncate -s 0 /etc/portage/package.keywords/zzz-autounmask
-	truncate -s 0 /etc/portage/package.use/zzz-autounmask
-	truncate -s 0 /etc/portage/package.unmask/zzz-autounmask
+truncate -s 0 /etc/portage/package.accept_keywords/zzz-autounmask
+truncate -s 0 /etc/portage/package.keywords/zzz-autounmask
+truncate -s 0 /etc/portage/package.use/zzz-autounmask
+truncate -s 0 /etc/portage/package.unmask/zzz-autounmask
+FEATURES=-test emerge --update --newuse --deep --with-bdeps=y @world
+emerge --depclean -q --with-bdeps=y
