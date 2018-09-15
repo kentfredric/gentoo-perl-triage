67b66a71b8c4b3d59e8b74fee24eeedfa7f9199c
Kent Fredric <kentnl@gentoo.org>
2018-09-16 09:31:37 +1200

    Add hacks for feh testing

--- rules/media-gfx.pl
+++ rules/media-gfx.pl
@@ -6,14 +6,22 @@ use My::Ruleset::Register -category => 'media-gfx';
 use My::Ruleset::Utils qw/add_keywords add_use/;
 
 match(
-    qr/^(imagemagick)-\d/ => {
-        install => sub {
-          # test? ( corefonts truetype )
-          add_use('corefonts truetype' => [
-              'media-gfx/imagemagick'
-          ]);
-        },
+  qr/^(imagemagick)-\d/ => {
+    install => sub {
+
+      # test? ( corefonts truetype )
+      add_use( 'corefonts truetype' => [ 'media-gfx/imagemagick' ] );
     },
+  },
+);
+match(
+  qr/^feh-2\.(18|26|27)/ => {
+    install => sub {
+
+      # https://bugs.gentoo.org/474556
+      add_use( 'jpeg png gif' => [ 'media-libs/imlib2' ] );
+    },
+  },
 );
 
 1;
