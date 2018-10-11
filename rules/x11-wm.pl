ca261250f5ecf70f5398014d551b7affbd078665
Kent Fredric <kentnl@gentoo.org>
2018-10-11 16:01:39 +1300

    Add i3 hack for xorg test deps to i3 4.15*

--- rules/x11-wm.pl
+++ rules/x11-wm.pl
@@ -6,7 +6,7 @@ use My::Ruleset::Register -category => 'x11-wm';
 use My::Ruleset::Utils qw/add_keywords add_use/;
 
 match(
-    qr/^(i3)-4\.14\.\d/ => {
+    qr/^(i3)-4\.1[45]/ => {
         testdeps => sub {
           add_use('xephyr kdrive' => [
               'x11-base/xorg-server'
