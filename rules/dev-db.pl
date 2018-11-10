f2c6b3088af550120edaa57299a9c980296a5b00
Kent Fredric <kentnl@gentoo.org>
2018-11-11 10:25:08 +1300

    disable usersandbox for mysql tests

---
+++ rules/dev-db.pl
@@ -0,0 +1,18 @@
+package ruleset::dev::db;
+
+use strict;
+use warnings;
+
+use My::Ruleset::Register -category => 'dev-db';
+use My::Ruleset::Utils qw/disable_feature/;
+
+match(
+  qr/^mysql-\d/ => {
+    test => sub {
+
+      # usersandbox unsupported
+      disable_feature( 'usersandbox', [ 'dev-db/mysql' ] );
+    },
+  } );
+
+1;
