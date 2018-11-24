648e4b6d103a3d96c832d93b5105aff7876e38cb
Kent Fredric <kentnl@gentoo.org>
2018-11-24 20:50:35 +1300

    Tidy www-apps rules

--- rules/www-apps.pl
+++ rules/www-apps.pl
@@ -6,25 +6,24 @@ use My::Ruleset::Register -category => 'www-apps';
 use My::Ruleset::Utils qw/add_keywords add_use/;
 
 match(
-    qr/^(bugzilla)-\d/ => {
-        install => sub {
-            add_use( 'sqlite' => ['www-apps/bugzilla'] );
-        },
+  qr/^(bugzilla)-\d/ => {
+    install => sub {
+      add_use( 'sqlite' => [ 'www-apps/bugzilla' ] );
     },
+  },
 );
 
 match(
-    qr/^(rt)-\d/ => {
-        install => sub {
-            add_keywords(
-                '~amd64' => [
+  qr/^(rt)-\d/ => {
+    install => sub {
+      add_keywords(
+        '~amd64' => [
 
-                    # Perl 5.28 breaks <2.0.6
-                    '=dev-perl/Role-Tiny-2.0.6',
-                ]
-            );
-        }
-    },
+          # Perl 5.28 breaks <2.0.6
+          '=dev-perl/Role-Tiny-2.0.6',
+        ] );
+    }
+  },
 );
 
 match(
