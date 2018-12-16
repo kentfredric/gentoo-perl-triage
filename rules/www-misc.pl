3a8ad70b7de3c0dc6d8f54c00231c3afbae6d494
Kent Fredric <kentnl@gentoo.org>
2018-12-16 20:10:50 +1300

    Add workaround for bug #638348 for www-misc/vdradmin-am

---
+++ rules/www-misc.pl
@@ -0,0 +1,17 @@
+# vim: syntax=perl
+#
+package ruleset::www::misc;
+
+use My::Ruleset::Register -category => 'www-misc';
+use My::Ruleset::Utils qw/add_keywords add_use/;
+
+match(
+  qr/^vdradmin-am-\d/ => {
+    install => sub {
+      # https://bugs.gentoo.org/638348
+      add_keywords('~amd64' => ['=media-video/vdr-2.2.0-r3']);
+    }
+  }
+);
+
+1;
