37886f5eba857619a26c26d6f3a89f37322eae70
Kent Fredric <kentnl@gentoo.org>
2018-12-22 21:26:17 +1300

    Update hacks for webkit-gtk

--- rules/net-libs.pl
+++ rules/net-libs.pl
@@ -6,7 +6,7 @@ use My::Ruleset::Register -category => 'net-libs';
 use My::Ruleset::Utils qw/add_keywords add_use/;
 
 match(
-    qr/^(webkit-gtk)-\d/ => {
+    qr/^(webkit-gtk)-2\.22\.2/ => {
         install => sub {
           add_use('-webgl -opengl -egl' => [
               'net-libs/webkit-gtk'
@@ -17,5 +17,27 @@ match(
         },
     },
 );
+
+match(
+    qr/^(webkit-gtk)-2\.22\.4/ => {
+        install => sub {
+          add_use('-webgl -opengl egl gles2' => [
+              'net-libs/webkit-gtk'
+          ]);
+          add_use('-opengl -egl' => [
+              'media-libs/gst-plugins-bad'
+          ]);
+          add_use('gles2' => [
+              'media-libs/mesa',
+              'media-libs/gst-plugins-base',
+              'media-libs/gst-plugins-bad',
+          ]);
+        },
+        testdeps => sub {
+          add_use('-glamor xvfb' => ['x11-base/xorg-server']);
+        },
+    },
+);
+
 
 1;
