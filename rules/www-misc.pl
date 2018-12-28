35a1b95d6e2d21e6b6640a9b35b4c5895da965b2
Kent Fredric <kentnl@gentoo.org>
2018-12-29 11:55:09 +1300

    Add vdr hack for xxv

--- rules/www-misc.pl
+++ rules/www-misc.pl
@@ -14,4 +14,13 @@ match(
   }
 );
 
+match(
+  qr/^xxv-\d/ => {
+    install => sub {
+      # https://bugs.gentoo.org/638348
+      add_keywords('~amd64' => ['=media-video/vdr-2.2.0-r3']);
+    }
+  }
+);
+
 1;
